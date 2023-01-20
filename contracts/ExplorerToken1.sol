3c250c42329469601a7b4100b7bf092788a67157
SYSTEMS\zeeshan.alam <zeeshan.alam@systemsltd.com>
2023-01-20 17:24:24 +0500

    project init - hardhat

---
+++ contracts/ExplorerToken1.sol
@@ -0,0 +1,14 @@
+// contracts/ExplorerToken1.sol
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ExplorerToken1 is ERC721 {
+  address owner;
+
+  constructor(){
+
+  }
+}
