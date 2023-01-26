ddaa97ae7bca17a5da57efe399bacbf7dad0e065
SYSTEMS\zeeshan.alam <zeeshan.alam@systemsltd.com>
2023-01-26 12:02:46 +0500

    hardhar condig

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,23 +4,16 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const ExplorerToken = await hre.ethers.getContractFactory('ExplorerToken1');
+  const explorerToken = await ExplorerToken.deploy();
+  await explorerToken.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`Explorer token deployed: ${explorerToken}`);
+  console.log(`Explorer token deployed: ${explorerToken.address}`);
+  // npx hardhat run --network goerli scripts/deploy.js
 }
 
 // We recommend this pattern to be able to use async/await everywhere
